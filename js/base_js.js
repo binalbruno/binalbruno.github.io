4b70790d56b55ad23437e646280a67ac3fe060ad
binalbruno <binalbruno@gmail.com>
2024-12-23 23:34:04 +0100

    add hover on the hobbies page

---
+++ js/base_js.js
@@ -0,0 +1,31 @@
+// alert("Hello, welcome")
+        
+// let userName=prompt("What is your name?")
+// document.write("Hello "+userName+", welcome to my website")
+
+function myFunction() {
+    alert("Hello! "+userName);
+    document.getElementById("name-heading").innerHTML = userName
+}
+
+function hideContent() {
+    currentStatus=document.getElementById("contentID").style.display;
+    console.log(currentStatus)
+    if (currentStatus=="none") {
+        document.getElementById("contentID").style.display = "block";
+        document.getElementById("hideButton").innerHTML = "Hide Content";
+    } else {
+        document.getElementById("contentID").style.display = "none";
+        document.getElementById("hideButton").innerHTML = "Show Content";
+    }
+}
+
+function upDate(previewPic){
+    document.getElementById('imageDisplay').style.backgroundImage = "url('"+previewPic.src+"')";
+    document.getElementById('imageDisplay').innerHTML = previewPic.alt;
+}
+
+function unDo(){
+    document.getElementById('imageDisplay').style.backgroundImage = "url('')";
+    document.getElementById('imageDisplay').innerHTML = "Hover over an image below to display here.";
+}
